22b616653b5d219c827c372a021d70dd8a83da2a
Luca Falsina <lfalsina@gmail.com>
2014-06-20 02:06:46 +0200

    Added JavaDocs and removed public attribute to constructor of SecureDexClassLoader.

--- POCCodeLoading/src/it/necst/grabnrun/SecureDexClassLoader.java
+++ POCCodeLoading/src/it/necst/grabnrun/SecureDexClassLoader.java
@@ -2,12 +2,36 @@ package it.necst.grabnrun;
 
 import dalvik.system.DexClassLoader;
 
+/**
+ * A class loader that extends the default one provided by the Android
+ * system and it is used to load classes from Jar and Apk files 
+ * containing a classes.dex entry in a secure way.
+ * 
+ * In order to instantiate this class a call to the method createDexClassLoader
+ * from a SecureLoaderFactory object must be performed.
+ * 
+ * SecureDexClassLoader ensures integrity of loaded external remote 
+ * classes by comparing them with the developer certificate, which
+ * is retrieved, as a first implementation, by reverting the 
+ * package name of the loaded class and downloading the 
+ * certificate at that location.
+ * 
+ * Please note that in the current implementation certificate must be 
+ * saved at the described location as "certificate.pem", it must 
+ * fit all the requirements of a standard X.509 certificate, it must 
+ * be valid and it must have been used to sign the Jar or Apk, which
+ * contains the classes to be loaded.
+ * 
+ * If any of these previous requirements is violated no class is loaded 
+ * and this class immediately returns.
+ * 
+ * @author Luca Falsina
+ */
 public class SecureDexClassLoader extends DexClassLoader {
 
-	public SecureDexClassLoader(String dexPath, String optimizedDirectory,
-			String libraryPath, ClassLoader parent) {
+	SecureDexClassLoader(	String dexPath, String optimizedDirectory,
+							String libraryPath, ClassLoader parent) {
 		super(dexPath, optimizedDirectory, libraryPath, parent);
-		// TODO Auto-generated constructor stub
 	}
 
 	/* (non-Javadoc)
