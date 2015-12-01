11e29954e76e034ea0fac3a176f5dce182433dca
Luca Falsina <lfalsina@gmail.com>
2015-12-01 18:08:29 +0100

    Extract utility methods in the PackageNameHelper. Create a test class for it.

---
+++ gnr/app/src/main/java/it/necst/grabnrun/PackageNameHelper.java
@@ -0,0 +1,62 @@
+package it.necst.grabnrun;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import android.support.annotation.NonNull;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PackageNameHelper {
+    @VisibleForTesting static final String HTTPS_PROTOCOL_AS_STRING = "https";
+    @VisibleForTesting static final String DEFAULT_CERTIFICATE_FILE_NAME = "/certificate.pem";
+
+    private static final String DOT_AS_REGULAR_EXPRESSION = "\\.";
+    private static final int MINIMUM_NUMBER_OF_FIELDS_IN_A_PACKAGE_NAME = 2;
+    private static final int FIRST_LEVEL_DOMAIN_INDEX = 0;
+    private static final int SECOND_LEVEL_DOMAIN_INDEX = 1;
+
+    public static boolean isAValidPackageName(@NonNull String candidatePackageName) {
+        checkNotNull(candidatePackageName, "The candidate package name must not be null");
+
+        if (candidatePackageName.isEmpty()) return false;
+
+        String[] packageNameFields = candidatePackageName.split(DOT_AS_REGULAR_EXPRESSION);
+        if (packageNameFields.length < MINIMUM_NUMBER_OF_FIELDS_IN_A_PACKAGE_NAME) {
+            return false;
+        }
+
+        for (String packageNameField : packageNameFields) {
+            if (packageNameField.isEmpty())
+                return false;
+        }
+
+        return true;
+    }
+
+    public static URL revertPackageNameToURL(@NonNull String candidatePackageName)
+            throws MalformedURLException {
+        checkArgument(
+                isAValidPackageName(candidatePackageName),
+                "A valid package name must be provided");
+
+        // If a package name is valid, it has at least two fields.
+        String[] packageNameFields = candidatePackageName.split(DOT_AS_REGULAR_EXPRESSION);
+        StringBuilder urlAsStringBuilder = new StringBuilder(HTTPS_PROTOCOL_AS_STRING + "://")
+                .append(packageNameFields[SECOND_LEVEL_DOMAIN_INDEX])
+                .append(".")
+                .append(packageNameFields[FIRST_LEVEL_DOMAIN_INDEX]);
+
+        for (int fieldIndex = 2; fieldIndex < packageNameFields.length; fieldIndex++) {
+            urlAsStringBuilder.append("/")
+                    .append(packageNameFields[fieldIndex]);
+        }
+
+        urlAsStringBuilder.append(DEFAULT_CERTIFICATE_FILE_NAME);
+
+        return new URL(urlAsStringBuilder.toString());
+    }
+}
---
+++ gnr/app/src/test/java/it/necst/grabnrun/PackageNameHelperTest.java
@@ -0,0 +1,96 @@
+package it.necst.grabnrun;
+
+import static it.necst.grabnrun.PackageNameHelper.isAValidPackageName;
+import static it.necst.grabnrun.PackageNameHelper.revertPackageNameToURL;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+public class PackageNameHelperTest {
+
+    private static final String TEST_PACKAGE_NAME_WITH_AN_EMPTY_FIELD_BETWEEN_TWO_DOTS = "com..example";
+    private static final String TEST_PACKAGE_NAME_WITH_ONLY_ONE_FIELD = "com";
+
+    private static final String TEST_VALID_PACKAGE_NAME_WITH_TWO_FIELDS = "com.example";
+    private static final String TEST_VALID_PACKAGE_NAME_WITH_FOUR_FIELDS =
+            "com.example.polimi.application";
+
+    private static final String TEST_EXPECTED_URL_STRING_WITH_VALID_PACKAGE_NAME_WITH_TWO_FIELDS =
+            "https://example.com/certificate.pem";
+    private static final String TEST_EXPECTED_URL_STRING_WITH_VALID_PACKAGE_NAME_WITH_FOUR_FIELDS =
+            "https://example.com/polimi/application/certificate.pem";
+
+    @Test
+    public void givenAnEmptyPackageName_whenIsAValidPackageName_thenReturnFalse() {
+        // GIVEN
+        String emptyPackageName = "";
+
+        // WHEN
+        boolean isAValidPackageName = isAValidPackageName(emptyPackageName);
+
+        // THEN
+        assertFalse(isAValidPackageName);
+    }
+
+    @Test
+    public void givenAPackageNameWithAnEmptyFieldBetweenTwoDots_whenIsAValidPackageName_thenReturnFalse() {
+        // WHEN
+        boolean isAValidPackageName = isAValidPackageName(
+                TEST_PACKAGE_NAME_WITH_AN_EMPTY_FIELD_BETWEEN_TWO_DOTS);
+
+        // THEN
+        assertFalse(isAValidPackageName);
+    }
+
+    @Test
+    public void givenAPackageNameWithOnlyOneField_whenIsAValidPackageName_thenReturnFalse() {
+        // WHEN
+        boolean isAValidPackageName = isAValidPackageName(TEST_PACKAGE_NAME_WITH_ONLY_ONE_FIELD);
+
+        // THEN
+        assertFalse(isAValidPackageName);
+    }
+
+    @Test
+    public void givenAPackageNameWithAtLeastTwoFieldsSeparatedByASingleDot_whenIsAValidPackageName_thenReturnTrue() {
+        // WHEN
+        boolean isAValidPackageName = isAValidPackageName(TEST_VALID_PACKAGE_NAME_WITH_TWO_FIELDS);
+
+        // THEN
+        assertTrue(isAValidPackageName);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void givenANotValidPackageName_whenRevertPackageNameToURL_thenThrows() throws Exception {
+        // WHEN
+        revertPackageNameToURL(TEST_PACKAGE_NAME_WITH_ONLY_ONE_FIELD);
+    }
+
+    @Test
+    public void givenAValidPackageNameWithTwoFields_whenRevertPackageNameToURL_thenReturnsTheRevertedPackageNameAsDomainName() throws Exception {
+        // WHEN
+        URL revertPackageNameToURL = revertPackageNameToURL(TEST_VALID_PACKAGE_NAME_WITH_TWO_FIELDS);
+
+        // THEN
+        assertThat(
+                revertPackageNameToURL,
+                is(equalTo(new URL(TEST_EXPECTED_URL_STRING_WITH_VALID_PACKAGE_NAME_WITH_TWO_FIELDS))));
+    }
+
+    @Test
+    public void givenAValidPackageNameWithThreeOrMoreFields_whenRevertPackageNameToURL_thenReturnsTheRevertedTwoFieldsOfPackageNameAsDomainNameAndTheRestAsFilePart() throws Exception {
+        // WHEN
+        URL revertPackageNameToURL = revertPackageNameToURL(TEST_VALID_PACKAGE_NAME_WITH_FOUR_FIELDS);
+
+        // THEN
+        assertThat(
+                revertPackageNameToURL,
+                is(equalTo(new URL(TEST_EXPECTED_URL_STRING_WITH_VALID_PACKAGE_NAME_WITH_FOUR_FIELDS))));
+    }
+}
